7f77ebe501a39e72c5d40a62af8909aab732a787
Jerrald <jerraldwee@google.com>
2021-03-25 09:12:52 +0000

    added preview function as part of actionables in table
    Change-Id: I37e83d8d038913c1b72561bd0a99deb35a4dba55

--- creative/app/routes.py
+++ creative/app/routes.py
@@ -8,38 +8,80 @@ doc_ref=db.collection(u'Surveys')
 
 @app.route('/index')
 def index():
-    all_surveys= doc_ref.stream()
-
-    return render_template('index.html', all_surveys = all_surveys)
+  all_surveys= doc_ref.stream()
+  return render_template('index.html', all_surveys = all_surveys)
 
 @app.route('/surveycreation',methods=['GET','POST'])
 def surveycreation():
     form = QuestionForm()
 
     if form.validate_on_submit():
-        data = {
-                u'SurveyName': form.surveyname.data,
-                u'Question1': form.question1.data,
-                u'Answer1a': form.answer1a.data,
-                u'Answer1b': form.answer1b.data,
-                u'Answer1c': form.answer1c.data,
-                u'Answer1d': form.answer1d.data,
-                u'Question2': form.question2.data,
-                u'Answer2a': form.answer2a.data
-                }
+      data = {
+          u'SurveyName': form.surveyname.data,
+          u'Question1': form.question1.data,
+          u'Answer1a': form.answer1a.data,
+          u'Answer1b': form.answer1b.data,
+          u'Answer1c': form.answer1c.data,
+          u'Answer1d': form.answer1d.data,
+          u'Question2': form.question2.data,
+          u'Answer2a': form.answer2a.data
+      }
 
-        doc_ref = db.collection(u'Surveys').document()
-        doc_ref.set(data)
-        # Then query for documents
-        users_ref = db.collection(u'Surveys')
+      doc_ref = db.collection(u'Surveys').document()
+      doc_ref.set(data)
+      # Then query for documents
+      users_ref = db.collection(u'Surveys')
 
-        for doc in users_ref.stream():
-            print(u'{} => {}'.format(doc.id, doc.to_dict()))
-        flash(f"{form.surveyname.data} is created as {doc_ref.id}")
+      for doc in users_ref.stream():
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+      flash(f"{form.surveyname.data} is created as {doc_ref.id}")
 
-        return redirect(url_for('index'))
+      return redirect(url_for('index'))
 
     return render_template('questions.html', title='Survey Creation', form=form)
+
+@app.route('/creative/preview/<string:survey_id>', methods=['GET'])
+def preview_creative(survey_id):
+  db = firestore.Client()
+  doc_ref = db.collection(u'Surveys').document(survey_id)
+  survey_doc = doc_ref.get()
+  if survey_doc.exists:
+    survey = survey_doc.to_dict()
+    return render_template(
+        'creative.html',
+        survey=survey,
+        all_question_json=get_question_json(survey))
+  else:
+    flash('Survey not found')
+    return redirect(url_for('index'))
+
+def get_question_json(survey):
+  all_question_json = []
+  for i in range(1, 5):
+    question_text = survey.get('Question' + str(i), '')
+    options = []
+    next_question = {}
+    if question_text:
+      question = {
+          'id': i,
+          'type': 'SINGLE_OPTION',  # TODO handle MULTIPLE_OPTION as well
+          'text': question_text,
+          'options': options,
+          'next_question': next_question
+      }
+      for j in ['a', 'b', 'c', 'd', 'e']:
+        answer_text = survey.get('Answer' + str(i) + j, '')
+        if answer_text:
+          answer_id = j.capitalize()
+          options.append({
+              'id': answer_id,
+              'role': 'option',
+              'text': answer_text
+          })
+          next_question[answer_id] = 'end'
+      all_question_json.append(question)
+
+  return all_question_json
 
 @app.route('/delete',methods=["GET","DELETE"])
 def delete():
@@ -62,16 +104,16 @@ def edit():
         form.answer1c.data = edit_doc.get('Answer1c',)
         form.answer1d.data = edit_doc.get('Answer1d',)
     if form.validate_on_submit():
-        data = {
-                u'SurveyName': form.surveyname.data,
-                u'Question1': form.question1.data,
-                u'Answer1a': form.answer1a.data,
-                u'Answer1b': form.answer1b.data,
-                u'Answer1c': form.answer1c.data,
-                u'Answer1d': form.answer1d.data
-                }
-        edit_doc = db.collection(u'Surveys').document(docref_id)
-        edit_doc.update(data)
-        flash(f"Survey with ID: {docref_id} is edited")
-        return redirect(url_for('index'))
+      data = {
+          u'SurveyName': form.surveyname.data,
+          u'Question1': form.question1.data,
+          u'Answer1a': form.answer1a.data,
+          u'Answer1b': form.answer1b.data,
+          u'Answer1c': form.answer1c.data,
+          u'Answer1d': form.answer1d.data
+      }
+      edit_doc = db.collection(u'Surveys').document(docref_id)
+      edit_doc.update(data)
+      flash(f"Survey with ID: {docref_id} is edited")
+      return redirect(url_for('index'))
     return render_template('questions.html', form=form)

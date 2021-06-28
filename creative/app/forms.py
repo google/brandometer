2eb8c3eeb79c1780117bbbf8dd98a4054943e2c6
Brad Wilson <bradx@google.com>
2021-06-28 11:08:51 +0800

    Add license and copyright information
    As per gPS externalisation guide
    
    Change-Id: I4ab17454b84661059d12b34d8cf0e0e2401a0f82

--- creative/app/forms.py
+++ creative/app/forms.py
@@ -1,3 +1,16 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Description of QuestionForm: Generates a survey name field input and 5 questions and answers for customers to fill out.
 
 It would capture the input of the customers and create a survey based on user
--- receiver/main.py
+++ receiver/main.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """# Documentation of receiver: It takes in responses from end users that have # completed the creative deployed by customers and writes the required data into # BigQuery on Google Cloud Platform.
 
 # Description of receiver:
